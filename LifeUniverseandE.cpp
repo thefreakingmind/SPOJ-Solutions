990d10229afecb242f7b721e07e63d21ec5e90ab
Salman Siddiqui <salmansiddiqui100@gmail.com>
2018-06-30 12:57:48 +0530

    Fixed

--- LifeUniverseandE.cpp
+++ LifeUniverseandE.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
-  int n;
-  while(true)
-  {
-	cin >> n;
-	cout << n;
-	fflush(stdout);
-	if (n==42)
-	  break;
-  }
-  return 0;
+int main() {
+	int n;
+	while(true)
+	{
+		cin>>n;
+		cout << n << endl;
+		if(n==42)
+		{
+			break;
+		}
+	}
+	return 0;
 }
